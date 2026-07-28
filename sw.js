7362329fb6340511bd02e3958197ebdcf0b8ca74
LeoZim-dev <leozim2363@gmail.com>
2026-07-28 10:43:51 +0200

    update cache sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,35 @@
-const CACHE_NAME = 'macropad-v1';
-const urlsToCache = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'macropad-v2'; // On change le nom pour forcer la maj
+const assets = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+  self.skipWaiting(); // Force le nouveau Service Worker à s'activer immédiatement
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key); // Supprime l'ancien cache v1
+          }
+        })
+      );
+    })
   );
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
